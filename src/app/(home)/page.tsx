f37a8482a3f24f7336433eb96fa18a729206f41a
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-09-05 21:53:41 +0800

    setup trpc and implement TRPC Client

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,10 @@
+import { trpc } from "@/trpc/client";
 
 export default function Home() {
+  const {data}= trpc.hello.useQuery({text: "hello world from TRPC"});
   return (
     <div>
-      <h1>I will load video here</h1>
+      <h1>TRPC Say : {data?.greeting}</h1>
     </div>
   );
 }
