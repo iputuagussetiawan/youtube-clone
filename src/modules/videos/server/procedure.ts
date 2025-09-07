f159176ad666bc454a22054d250157450fc06d16
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-09-07 15:13:47 +0800

    create webhook to upload videos to MUX and integration with my database neon

--- src/modules/videos/server/procedure.ts
+++ src/modules/videos/server/procedure.ts
@@ -23,6 +23,8 @@ export const videosRouter = createTRPCRouter({
             .values({
                 userId,
                 title:"Untitled",
+                muxStatus:"waiting",
+                muxUploadId:upload.id
             })
             .returning();
         return{
