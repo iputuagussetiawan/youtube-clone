a0180a5599aa91da4c97ed31c8e3408ac5641cb2
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-09-07 09:11:24 +0800

    create query videos

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,8 +1,16 @@
+import { DEFAULT_LIMIT } from '@/constant';
+import StudioView from '@/modules/studio/ui/view/studio-view';
+import { HydrateClient, trpc } from '@/trpc/server';
 import React from 'react'
 
-const StudioPage = () => {
+const StudioPage = async() => {
+    void trpc.studio.getMany.prefetchInfinite({
+        limit:DEFAULT_LIMIT
+    });
     return (
-        <div>Studio page</div>
+        <HydrateClient>
+            <StudioView />
+        </HydrateClient>
     )
 }
 
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 
 import { desc } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
 
 export const users= pgTable("users",{
@@ -12,6 +13,10 @@ export const users= pgTable("users",{
     //TODO: add banner field
 },(t)=>[uniqueIndex("clerk_id_idx").on(t.clerkId)]);
 
+export const userRelations=relations(users,({many})=>({
+    videos:many(videos),
+}))
+
 export const categories= pgTable("categories",{
     id:uuid("id").primaryKey().defaultRandom(),
     name:text("name").notNull().unique(),
@@ -19,3 +24,32 @@ export const categories= pgTable("categories",{
     createdAt:timestamp("created_at").notNull().defaultNow(),
     updatedAt:timestamp("updated_at").notNull().defaultNow(),
 },(t)=>[uniqueIndex("name_idx").on(t.name)]);
+
+export const categoryRelations = relations(categories, ({ many }) => ({
+    videos: many(videos),
+}));
+
+export const videos= pgTable("videos",{
+    id:uuid("id").primaryKey().defaultRandom(),
+    title:text("title").notNull(),
+    description:text("description"),
+    userId:uuid("user_id").notNull().references(() => users.id,{
+        onDelete:"cascade"
+    }),
+    categoryId:uuid("category_id").references(() => categories.id, {
+        onDelete:"set null"
+    }),
+    createdAt:timestamp("created_at").notNull().defaultNow(),
+    updatedAt:timestamp("updated_at").notNull().defaultNow()
+});
+
+export const videoRelations=relations(videos,({one})=>({
+    user:one(users,{
+        fields:[videos.userId],
+        references:[users.id]
+    }),
+    category:one(categories,{
+        fields:[videos.categoryId],
+        references:[categories.id]
+    }),
+}))
