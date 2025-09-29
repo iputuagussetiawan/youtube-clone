55e3e3891b15715c371e56b570a4521fc37cd634
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-09-29 20:59:17 +0800

    subscription functionality

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import SubscriptionButton from "@/modules/subscriptions/ui/components/subscription-button";
 import UserInfo from "@/modules/users/ui/components/user-info";
+import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription";
 
 interface VideoOwnerProps {
     user:VideoGetOneOutput["user"];
@@ -12,7 +13,12 @@ interface VideoOwnerProps {
 }
 
 const VideoOwner = ({ user, videoId }:VideoOwnerProps) => {
-    const {userId:clerkUserId}=useAuth()
+    const {userId:clerkUserId}=useAuth();
+    const {isPending, onClick}=useSubscription({
+        userId:user.id,
+        isSubscribed:user.viewerSubscribed,
+        fromVideoId:videoId
+    })
     return (
         <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
             <Link href={`/users/${user.id}`}>
@@ -37,9 +43,9 @@ const VideoOwner = ({ user, videoId }:VideoOwnerProps) => {
                 </Button>
             ):(
                 <SubscriptionButton 
-                    onClick={() => {}}
-                    disabled={false}
-                    isSubscribed={false}
+                    onClick={onClick}
+                    disabled={isPending}
+                    isSubscribed={user.viewerSubscribed}
                     className="flex-none"
                 />
             )}
