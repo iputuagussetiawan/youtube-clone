327c2be60b8019981aba2063dc4ba85803c993cc
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-10-04 12:01:37 +0800

    comment functionality

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -2,14 +2,35 @@ import Link from "next/link";
 import { CommentsGetManyOutput } from "../../types";
 
 interface CommentItemProps {
-    comment: CommentsGetManyOutput[number];
+    comment: CommentsGetManyOutput["items"][number];
 }   
 
 import React from 'react'
 import UserAvatar from "@/components/user-avatar";
 import { formatDistanceToNow } from "date-fns";
+import { trpc } from "@/trpc/client";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { MessageSquareIcon, MoreVerticalIcon, Trash, Trash2Icon } from "lucide-react";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import { toast } from "sonner";
 
 const CommentItem = ({comment}:CommentItemProps) => {
+    const clerk=useClerk();
+    const {userId}=useAuth();
+    const utils=trpc.useUtils();
+    const remove=trpc.comments.remove.useMutation({
+        onSuccess: () => {
+            toast.success("Comment deleted");
+            utils.comments.getMany.invalidate({videoId:comment.videoId});
+        },
+        onError: (error) => {
+            toast.error("Something went wrong");
+            if (error.data?.code === "UNAUTHORIZED") {
+                clerk.openSignIn();
+            }
+        },
+    });
     return (
         <div>
             <div className="flex gap-4">
@@ -26,7 +47,29 @@ const CommentItem = ({comment}:CommentItemProps) => {
                     </div>
                     </Link>
                     <p className="text-sm">{comment.value}</p>
+                    
                 </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" size="icon" className="size-8" >
+                            <MoreVerticalIcon />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => {}}>
+                            <MessageSquareIcon className="size-4" />
+                            Replay
+                        </DropdownMenuItem>
+                        {
+                            comment.user.clerkId === userId && (
+                                <DropdownMenuItem onClick={() => remove.mutate({id:comment.id})}>
+                                    <Trash2Icon className="size-4" />
+                                    Remove
+                                </DropdownMenuItem>
+                            )
+                        }
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     )
--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,7 +1,10 @@
 "use client";
+import InfiniteScroll from '@/components/infinite-scroll';
+import { DEFAULT_LIMIT } from '@/constant';
 import CommentForm from '@/modules/comments/ui/components/comment-form';
 import CommentItem from '@/modules/comments/ui/components/comment-item';
 import { trpc } from '@/trpc/client';
+import { Loader2Icon } from 'lucide-react';
 import React from 'react'
 
 interface CommentsSectionProps {
@@ -10,23 +13,42 @@ interface CommentsSectionProps {
 
 export const CommentsSection = ({videoId}:CommentsSectionProps) => {
     return (
-        <React.Suspense fallback={<div>Loading...</div>}>
+        <React.Suspense fallback={<CommentsSectionSkeleton />}>
             <CommentsSectionSuspense videoId={videoId}/>
         </React.Suspense>
     )
 }
 
+const CommentsSectionSkeleton = () => {
+    return (
+        <div className='mt-6 flex justify-center items-center'>
+            <Loader2Icon className='text-muted-foreground size-7 animate-spin'/>
+        </div>
+    )
+}
+
 const CommentsSectionSuspense = ({videoId}:CommentsSectionProps) => {
-    const [comments]=trpc.comments.getMany.useSuspenseQuery({videoId:videoId});
+    const [comments, query]=trpc.comments.getMany.useSuspenseInfiniteQuery({
+        videoId:videoId,
+        limit:DEFAULT_LIMIT
+    },{
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+    });
     return (
         <div className='mt-6'>
             <div className="flex flex-col gap-6">
-                <h2>0 Comments</h2>
+                <h2 className='text-xl font-semibold'>{comments.pages[0].totalCount} Comments</h2>
                 <CommentForm videoId={videoId}/>
                 <div className="flex flex-col gap-4 mt-2">
-                    {comments.map((comment) => (
+                    {comments.pages.flatMap((page) => page.items).map((comment) => (
                         <CommentItem key={comment.id} comment={comment}/>
                     ))}
+                    <InfiniteScroll
+                        isManual
+                        hasNextPage={query.hasNextPage}
+                        isFetchingNextPage={query.isFetchingNextPage}
+                        fetchNextPage={query.fetchNextPage}
+                    />
                 </div>
             </div>
         </div>
